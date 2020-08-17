32e3964470cdcff58c90343718c667762c1ba5bf
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2020-08-17 12:44:28 +0200

    Initial commit.

--- README.md
+++
@@ -1,4 +0,0 @@
-# AnubisPixelmonTournaments
-
-- https://trello.com/c/0wmq5CqB/228-tournament-recode
-- https://pixelmonmod.com/wiki/Tournaments
---
+++ src/main/java/your/packagee/here/PluginName.java
@@ -0,0 +1,22 @@
+package your.packagee.here;
+
+import org.spongepowered.api.plugin.Dependency;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+    id = "",
+    name = "",
+    version = "",
+    description = "",
+    authors = { "" },
+    dependencies = {
+        @Dependency(id = ""),
+        @Dependency(id = "", optional = true)
+    }
+)
+public class PluginName
+{
+
+
+
+}
